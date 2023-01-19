eb2437a258442b6a3c5d9105c3bbb665e4c37138
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-01-19 14:59:58 +0900

    앱 웹뷰, 아이콘

--- App.js
+++ App.js
@@ -1,15 +1,59 @@
-import { View, Text } from "react-native";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import WebView from "react-native-webview";
+import { useEffect, useRef, useState } from "react";
+import { BackHandler } from "react-native";
 
 export default function App() {
+  const webview = useRef();
+  const [navState, setNavState] = useState(null);
+  const [currentUrl, setCurrentUrl] = useState("https://seriesbox.netlify.app");
+
+  const onAndroidBackPress = () => {
+    if (webview.current) {
+      if (!navState?.canGoBack) {
+        exitTimer += 1;
+        if (exitTimer === 1) {
+          onBackToast();
+        }
+        if (exitTimer === 1) {
+          setTimeout(() => (exitTimer = 0), 2000);
+        } else if (exitTimer === 2) {
+          BackHandler.exitApp();
+          return false;
+        }
+      } else {
+        if (navState) {
+          webview.current.goBack();
+        } else {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  //react native android quit to press backbutton twice
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", onAndroidBackPress);
+
+    return () =>
+      BackHandler.removeEventListener("hardwareBackPress", onAndroidBackPress);
+  }, [navState]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <SafeAreaProvider>
+      <StatusBar barStyle={"dark-content"} />
+      <SafeAreaView style={{ flex: 1 }}>
+        <WebView
+          source={{ uri: currentUrl }}
+          onNavigationStateChange={setNavState}
+          ref={webview}
+          onLoad={(props) => setNavState(props.nativeEvent)}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
